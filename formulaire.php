2820ac91431e24ed05d9819d08ed86360ee67ac0
Assane Ali Boye <aalijr97@gmail.com>
2021-08-04 21:40:55 +0200

    acces au formulaire si login et password sont verifiés

--- formulaire.php
+++ formulaire.php
@@ -11,20 +11,32 @@
 
     <?php
         include_once('menu.php');
+
+        session_start();
+
+        if($_SESSION['login']=="admin" AND $_SESSION['password']=="admin"){
     ?>
-    
-    <form action="inscription.php" method="POST">
-        nom: <input type="text" name="nom" id="nom" required>
-        <span id="nomindic"></span>
-        prenom: <input type="text" name="prenom" id="prenom" required>
-        <span id="prenomindic"></span>
-        email: <input type="text" name="email" id="email" required>
-        <span id="emailindic"></span>
-        pays: <input type="text" name="pays" id="pays" required>
-        <span id="paysindic"></span>
-        <input type="submit" value="envoyer" id="send">
-    </form>
-    
+            <form action="inscription.php" method="POST">
+            nom: <input type="text" name="nom" id="nom" required>
+            <span id="nomindic"></span>
+            prenom: <input type="text" name="prenom" id="prenom" required>
+            <span id="prenomindic"></span>
+            email: <input type="text" name="email" id="email" required>
+            <span id="emailindic"></span>
+            pays: <input type="text" name="pays" id="pays" required>
+            <span id="paysindic"></span>
+            <input type="submit" value="envoyer" id="send">
+        </form>
+    <?php      
+        }
+        else{
+    ?>
+                <p> vs n'avez pas acces a ce site </p>
+                <a href="index.php"> se connecter</a>
+    <?php
+
+        }
+    ?>  
     
 
 </body>
